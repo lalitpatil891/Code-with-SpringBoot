366f0eaf7f98843679f4beb4ff7246572d329739
Lalit Patil <lp613408@gmail.com>
2025-06-30 11:55:01 +0530

    feat: add WishMessageGenerator bean with constructor and setter injection

---
+++ Programs/springXmlInjectionDemo/src/com/example/beans/WishMessageGenerator.java
@@ -0,0 +1,43 @@
+package com.example.beans;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+public class WishMessageGenerator {
+
+	private LocalTime time;  //dependency
+	private LocalDate date;  //dependency
+	
+	//Constructor Injection
+	public WishMessageGenerator(LocalTime time, LocalDate date) {
+		System.out.println("WishMessageGenerator: constructor Called");
+		this.time = time;
+		this.date = date;
+	}
+	
+	//setter injection
+	public void setTime(LocalTime time) {
+		System.out.println("WishMessageGenerator: setTime() called");
+		this.time = time;
+	}
+	
+	public void setDate(LocalDate date) {
+		System.out.println("WishMessageGenerator: setDate() called");
+		this.date = date;
+	}
+	
+	//Method to generate message
+	public String generateWish(String name) {
+		
+		int hour = time.getHour();
+		String msg;
+		
+		if(hour<12) msg = "Good Morning...";
+		else if(hour<17) msg = "Good Afternoon...";
+		else if(hour<20) msg = "Good Evening...";
+		else msg = "Good Night...";
+		
+		return msg +" , "+name+"!, Today's date: "+date;
+		
+	}	
+}
