df2b1fb3b6d562833d35c68cf63a8d3f6d616f7a
Lalit Patil <lp613408@gmail.com>
2025-07-08 23:07:03 +0530

    feat: initialize Spring Boot project with web dependency

---
+++ Programs/tea-shop-bill/tea-shop-bill/src/main/java/com/teashop/TeaShopBillApplication.java
@@ -0,0 +1,13 @@
+package com.teashop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeaShopBillApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeaShopBillApplication.class, args);
+	}
+
+}
---
+++ Programs/tea-shop-bill/tea-shop-bill/src/test/java/com/teashop/TeaShopBillApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teashop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeaShopBillApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
