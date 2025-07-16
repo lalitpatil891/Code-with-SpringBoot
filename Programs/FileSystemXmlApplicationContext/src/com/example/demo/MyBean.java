9099cbd80ca3e40afaedfe166e0e2f865db4eed7
Lalit Patil <lp613408@gmail.com>
2025-07-16 11:58:51 +0530

    Add MyBean class for ClassPathXmlApplicationContext example

---
+++ Programs/FileSystemXmlApplicationContext/src/com/example/demo/MyBean.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+public class MyBean {
+	
+	public void sayHello() {
+		System.out.println("Hello from MyBean (FileSystemApplicationContext)");
+	}
+}
+
+
+/*Output->
+
+Hello from MyBean (FileSystemApplicationContext)
+
+*/
