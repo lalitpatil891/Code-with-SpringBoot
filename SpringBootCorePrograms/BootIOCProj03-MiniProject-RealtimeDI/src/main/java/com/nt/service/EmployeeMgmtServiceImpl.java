db263123c53d87540cc407d625c8e48269278100
Lalit Patil <lp613408@gmail.com>
2025-08-07 12:58:09 +0530

    Added service layer with business logic for gross and net salary calculation

---
+++ SpringBootCorePrograms/BootIOCProj03-MiniProject-RealtimeDI/src/main/java/com/nt/service/EmployeeMgmtServiceImpl.java
@@ -0,0 +1,34 @@
+package com.nt.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.nt.dao.IEmployeeDAO;
+import com.nt.model.Employee;
+
+@Service("empService")
+public class EmployeeMgmtServiceImpl implements IEmployeeMgmtService {
+	@Autowired
+	private IEmployeeDAO empDAO;
+	
+	public List<Employee> fetchEmployeesByDesgs(String desg1, String desg2, String desg3) throws Exception {
+		// convert thhe desgs (inputs to upper case letter) (Business logic)
+		desg1 = desg1.toUpperCase();
+		desg2 = desg2.toUpperCase();
+		desg3 = desg3.toUpperCase();
+
+		// use DAO
+		List<Employee> list = empDAO.showEmployeeByDesgs(desg1, desg2, desg3);
+		// calculate gross Salary and NetSalary(business Logic)
+
+		list.forEach(emp -> {
+			emp.setGrossSalary(emp.getSalary() + (emp.getSalary() * 0.4));
+			emp.setNetSalary(emp.getGrossSalary() - (emp.getGrossSalary() * 0.15));
+
+		});
+		return list;
+	}
+
+}
